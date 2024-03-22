eb4f57dce31d28dabf9d5633f9a0ef7dea5ce3e8
Luis Chagas <luishenriquech@gmail.com>
2024-03-22 00:02:29 -0300

    [WIP] Creating deposits steps

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,8 @@ from selenium.webdriver import Firefox, Chrome, Edge
 from ipdb import post_mortem
 from selenium.webdriver.support.ui import WebDriverWait
 
-def before_all(context):
+
+def before_scenario(context, scenario):
     browser =  context.config.userdata.get('browser')
 
     browsers = {
@@ -11,15 +12,14 @@ def before_all(context):
         'edge': Edge
     }
     context.browser = browsers[browser]()
-
-def before_scenario(context, scenario):
     context.browser.implicitly_wait(10)
     
 
 def after_step(context, step):
+    context.browser.implicitly_wait(10)
     if context.config.userdata.getbool("debug") and step.status == 'failed':
        post_mortem(step.exc_traceback)
     
 
-def after_scenario(context, scenario):
-    context.browser.quit()
+#def after_scenario(context, scenario):
+    #context.browser.quit()
