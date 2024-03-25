db2a38f5c81c639163e4a4749bb6eef97c62877b
Luis Chagas <luishenriquech@gmail.com>
2024-03-24 22:38:07 -0300

    deposit update

--- features/steps/deposits_steps.py
+++ features/steps/deposits_steps.py
@@ -9,6 +9,7 @@ from urllib.parse import urlparse, urljoin
 
 DEPOSIT_URL = "https://test-bees.herokuapp.com/deposits"
 SUCCESSFUL_MSG_DEFAULT = 'Deposit was successfully created.'
+SUCCESSFUL_MSG_UPDATE = 'Deposit was successfully updated.'
 DEPOSIT_ID = "deposits/189"
 
 @given('stay on "{deposit_page}" session')
@@ -26,8 +27,12 @@ def step_impl(context, deposit_edit):
     print(final_url)  # Output: 
     context.browser.get(final_url)
 
-    link_check_dp = context.browser.find_element(By.LINK_TEXT, 'Edit this deposit').text
-    assert link_check_dp in deposit_edit
+    edit_deposit_button = context.browser.find_element(By.XPATH, '/html/body/div/div[2]/a[1]')
+    edit_deposit_button.click()
+    
+    #assert Title from Edit page
+    title = context.browser.find_element(By.TAG_NAME, 'h1').text
+    assert title in deposit_edit
     
 
 @when('create a new deposit')
@@ -57,6 +62,42 @@ def create_deposit(context):
     create_deposit_button.click()
 
 
+@when(u'edit a deposit')
+def step_impl(context):
+
+
+    edit_deposits = loads(context.text)
+    # name
+    deposit_edit_name = context.browser.find_element(By.ID, 'deposit_name')
+    deposit_edit_name.clear()
+    deposit_edit_name.send_keys(edit_deposits['name'])
+
+    #address
+    deposit_edit_address = context.browser.find_element(By.ID, 'deposit_address')
+    deposit_edit_address.clear()
+    deposit_edit_address.send_keys(edit_deposits['address'])
+
+    #city
+    deposit_edit_city = context.browser.find_element(By.ID, 'deposit_city')
+    deposit_edit_city.clear()
+    deposit_edit_city.send_keys(edit_deposits['city'])
+
+    #state
+    deposit_edit_state = context.browser.find_element(By.ID, 'deposit_state')
+    deposit_edit_state.clear()
+    deposit_edit_state.send_keys(edit_deposits['state'])
+
+    #zipcode
+    deposit_edit_zipcode = context.browser.find_element(By.ID, 'deposit_zipcode')
+    deposit_edit_zipcode.clear()
+    deposit_edit_zipcode.send_keys(edit_deposits['zipcode'])
+
+    #Apply update
+    update_deposit_button = context.browser.find_element(By.XPATH, '/html/body/div/form/div[2]/input')
+    update_deposit_button.click()
+
+
+
 @then('the deposits were created successful')
 def verify_deposit(context):
     
@@ -72,12 +113,12 @@ def verify_deposit(context):
     back_to_deposit_link.click()
 
 
-@when(u'edit a deposit')
-def step_impl(context):
-    ...
-
-
 
 @then(u'the deposits were edited successful')
 def step_impl(context):
-    ...
+    successful_msg_deposit_update = context.browser.find_element(By.XPATH, '/html/body/div/p').text
+    assert successful_msg_deposit_update in SUCCESSFUL_MSG_UPDATE
+    print(successful_msg_deposit_update)
+    
+    back_to_deposit_link = context.browser.find_element(By.LINK_TEXT, 'Back to deposits')
+    back_to_deposit_link.click()
